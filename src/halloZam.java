40efd1637dafb4768e9847daf1df1ef4944c4c00
jama_ <jama_zamzam@hotmail.com>
2025-05-28 09:28:48 +0200

    Testning commit BFF

--- src/halloZam.java
+++ src/halloZam.java
@@ -2,6 +2,8 @@ public class halloZam {
     public static void main(String[] args) {
         System.out.println("Zamzam");
         System.out.println("ZamzamJama");
+        System.out.println("ZamzamJamaYusuf");
+
 
     }
 }
